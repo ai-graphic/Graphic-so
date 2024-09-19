a725c313061993eab76d1a8082aa76b5561e884c
rohit-sama <rohit9804singh@gmail.com>
2024-09-19 13:22:52 +0530

    feat(app) : update icon to show in metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,45 @@
-import type {Metadata} from "next";
-import {Inter, DM_Sans} from "next/font/google";
+import type { Metadata } from "next";
+import { Inter, DM_Sans } from "next/font/google";
 import "./globals.css";
-import {ThemeProvider} from "@/hooks/theme-provider";
-import {ClerkProvider} from "@clerk/nextjs";
+import { ThemeProvider } from "@/hooks/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 import ModalProvider from "@/hooks/modal-provider";
-import {Toaster} from "@/components/ui/sonner";
-import {BillingProvider} from "@/hooks/billing-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { BillingProvider } from "@/hooks/billing-provider";
 
-const font = DM_Sans({subsets: ["latin"]});
+const font = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "graphic : AI-Powered Workflow Automation",
-    description: "graphic.so empowers users to automate workflows and leverage a suite of AI tools. Ideal for building low-code chatbots and integration platforms for efficient task management and enhanced customer support."
+  title: "graphic : AI-Powered Workflow Automation",
+  description:
+    "graphic.so empowers users to automate workflows and leverage a suite of AI tools. Ideal for building low-code chatbots and integration platforms for efficient task management and enhanced customer support.",
+    icons: "/icon.ico",
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <ClerkProvider
-            publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-        >
-            <html lang="en">
-            <body className={font.className}>
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="dark"
-                enableSystem
-                disableTransitionOnChange
-            >
-                
-                
-                    <ModalProvider>
-                        {children}
-                        <Toaster/>
-                    </ModalProvider>
-            </ThemeProvider>
-
-            </body>
-            </html>
-        </ClerkProvider>
-    );
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ModalProvider>
+              {children}
+              <Toaster />
+            </ModalProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
